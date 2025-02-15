66c59b39563a01ee93c2fbc9fcad04f5ab1d785d
djbayman <djaballahayman20@gmail.com>
2025-02-15 20:40:24 +0100

    code enhancement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,26 @@
 "use strict";
 
-// Animation on scroll
+// Utility Functions
+function showElement(ele) {
+  ele.classList.add("show");
+}
+
+function hideElement(ele) {
+  ele.classList.remove("show");
+}
+
+function toggleActiveState(elements, targetElement) {
+  elements.forEach(
+    (item) => item !== targetElement && item.classList.remove("active")
+  );
+  targetElement.classList.toggle("active");
+}
+
+// Animation on Scroll
 document.addEventListener("DOMContentLoaded", () => {
   const sections = document.querySelectorAll("#aos");
-
   const observer = new IntersectionObserver(
-    (entries, observer) => {
+    (entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.setAttribute("data-visible", "true");
@@ -16,54 +31,40 @@ document.addEventListener("DOMContentLoaded", () => {
     { threshold: 0.1 }
   );
 
-  sections.forEach((section) => {
-    observer.observe(section);
-  });
+  sections.forEach((section) => observer.observe(section));
 });
 
-// control the navbar
+// Navbar Control
 const navItems = document.querySelectorAll("#navmenu ul li");
+navItems.forEach((navItem) => {
+  navItem.addEventListener("click", () => toggleActiveState(navItems, navItem));
+});
+
+// Sidebar Control
 const hamburger = document.querySelector("#hamburger");
 const aside = document.querySelector("aside");
-const asideContent = document.querySelector("aside .aside-content");
 const asideOverlay = document.querySelector("aside .aside-overlay");
 const removeAside = document.querySelector("aside .remove-aside");
 const asideNavItems = document.querySelectorAll("#asideList li");
-const asideDropMenu = document.querySelector("#asideList li");
 
-// open the navItem and close the rest
-navItems.forEach((navItem) => {
-  navItem.addEventListener("click", () => {
-    navItem.classList.toggle("active");
-    navItems.forEach((item) => {
-      if (item !== navItem) {
-        item.classList.remove("active");
-      }
-    });
-  });
-});
+function toggleSidebar(show) {
+  show ? showElement(aside) : hideElement(aside);
+  document.body.classList.toggle("no-scroll", show);
+}
 
-// open the sidebar on mobile
-document.addEventListener("DOMContentLoaded", () => {
-  hamburger.addEventListener("click", () => {
-    showElement(aside);
-    serviceListMob.classList.remove("show");
-    document.body.classList.add("no-scroll");
-  });
-});
+// Hamburger Toggle
+hamburger.addEventListener("click", () => toggleSidebar(true));
+[removeAside, asideOverlay].forEach((el) =>
+  el.addEventListener("click", () => toggleSidebar(false))
+);
 
 asideNavItems.forEach((aNavItem) => {
-  aNavItem.addEventListener("click", () => {
-    aNavItem.classList.toggle("active");
-    asideNavItems.forEach((item) => {
-      if (item !== aNavItem) {
-        item.classList.remove("active");
-      }
-    });
-  });
+  aNavItem.addEventListener("click", () =>
+    toggleActiveState(asideNavItems, aNavItem)
+  );
 });
 
-// close the opened navitems
+// Close nav items on outside click
 window.addEventListener("click", (e) => {
   navItems.forEach((item) => {
     if (e.target !== item && e.target !== item.children[0]) {
@@ -72,24 +73,7 @@ window.addEventListener("click", (e) => {
   });
 });
 
-function showElement(ele) {
-  ele.classList.add("show");
-}
-function hideElement(ele) {
-  ele.classList.remove("show");
-}
-
-// remove the sidebar
-removeAside.addEventListener("click", () => {
-  hideElement(aside);
-  document.body.classList.remove("no-scroll");
-});
-asideOverlay.addEventListener("click", () => {
-  hideElement(aside);
-  document.body.classList.remove("no-scroll");
-});
-
-// render the lists
+// Render dynamic lists
 document.addEventListener("DOMContentLoaded", () => {
   const listItems = [
     "Salles des fêtes",
@@ -103,6 +87,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "Coiffure & Beauté",
     "Bijoux et accessoires",
   ];
+
   const listItemsTwo = [
     "Alger",
     "Oran",
@@ -159,159 +144,151 @@ document.addEventListener("DOMContentLoaded", () => {
     "El Meniaa",
   ];
 
+  // Input fields
   const inputTypeService = document.querySelector(".type-service");
   const inputLocationService = document.querySelector(".location-service");
+
+  // Desktop lists
   const typeServiceList = document.querySelector(".type-service-list");
   const locationServiceList = document.querySelector(".location-service-list");
-  const serviceListMob = document.querySelector(".list-appear");
+
+  // Mobile lists
+  const serviceListMobUl = document.querySelector(".type-service-list-mob-v");
+  const serviceListMobUlTow = document.querySelector(
+    ".location-service-list-mob-v"
+  );
+
+  // Mobile container and overlay
+  const serviceListMobContainer = document.querySelector(".list-appear");
   const serviceListMobOverlay = document.querySelector(
     ".list-appear .list-overlay"
   );
-  const serviceListMobUl = document.querySelector(
-    ".list-appear .type-service-list-mob-v"
-  );
-  const serviceListMobUlTow = document.querySelector(
-    ".list-appear .location-service-list-mob-v"
-  );
-  const titleService = document.querySelector(".list-appear .title-service h4");
-  const inputService = document.querySelector(".list-appear input");
   const removeServiceList = document.querySelector(
     ".list-appear .title-service img"
   );
+  const titleService = document.querySelector(".list-appear .title-service h4");
+  const inputService = document.querySelector(".list-appear input");
 
-  // Function to handle input focus event on mobile devices
-  function handleInputFocus(event) {
-    // Prevent the keyboard from showing up on mobile devices
-    if (isMobileDevice()) {
-      event.preventDefault();
+  // Prevent keyboard on mobile devices
+  const handleInputFocus = (e) => {
+    if (/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+      e.preventDefault();
     }
-  }
+  };
 
-  // Function to detect if the user is on a mobile device
-  function isMobileDevice() {
-    return /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
-  }
+  // Add readonly attribute based on screen size
+  const addReadonlyAttribute = () => {
+    const isMobile = window.matchMedia("(max-width: 768px)").matches;
+    inputTypeService.readOnly = isMobile;
+    inputLocationService.readOnly = isMobile;
+  };
 
-  // Function to add readonly attribute based on screen size
-  function addReadonlyAttribute() {
-    if (window.matchMedia("(max-width: 768px)").matches) {
-      inputTypeService.setAttribute("readonly", true);
-      inputLocationService.setAttribute("readonly", true);
-    } else {
-      inputTypeService.removeAttribute("readonly");
-      inputLocationService.removeAttribute("readonly");
-    }
-  }
-
-  // Call the function to add readonly attribute on page load
   addReadonlyAttribute();
-
-  // Call the function to add readonly attribute on window resize
   window.addEventListener("resize", addReadonlyAttribute);
 
-  // show the list when focusing on the input
+  // Function to hide all lists
+  const hideAllLists = () => {
+    hideElement(typeServiceList);
+    hideElement(locationServiceList);
+    hideElement(serviceListMobUl);
+    hideElement(serviceListMobUlTow);
+    hideElement(serviceListMobContainer);
+  };
+
+  // Function to show a specific list
+  const showList = (list, mobileList, title, placeholder, items) => {
+    hideAllLists(); // Hide all lists before showing the current one
+    if (window.matchMedia("(max-width: 768px)").matches) {
+      // Mobile view
+      showElement(mobileList);
+      showElement(serviceListMobContainer);
+      titleService.textContent = title;
+      inputService.placeholder = placeholder;
+      inputService.value = "";
+      renderList(inputService, mobileList, items);
+    } else {
+      // Desktop view
+      showElement(list);
+    }
+    scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    document.body.classList.add("no-scroll");
+  };
+
+  // Show type service list
   inputTypeService.addEventListener("focus", (e) => {
     handleInputFocus(e);
-    serviceListMobUlTow.style.display = "none";
-    serviceListMobUl.style.display = "block";
-
-    inputService.value = "";
-
-    showElement(typeServiceList);
-    showElement(serviceListMob);
-    hideElement(locationServiceList);
-
-    titleService.textContent = "Que Chercher Vous ?";
-    inputService.placeholder = "Chercher un service";
-
-    serviceListMobUl.innerHTML = "";
-    listItems.forEach((item) => {
-      const li = document.createElement("li");
-      li.textContent = item;
-      serviceListMobUl.appendChild(li);
-    });
-
-    scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-    document.body.classList.add("no-scroll");
+    showList(
+      typeServiceList,
+      serviceListMobUl,
+      "Que Chercher Vous ?",
+      "Chercher un service",
+      listItems
+    );
   });
 
+  // Show location service list
   inputLocationService.addEventListener("focus", (e) => {
     handleInputFocus(e);
-    serviceListMobUl.style.display = "none";
-    serviceListMobUlTow.style.display = "block";
-    inputService.value = "";
-    showElement(locationServiceList);
-    showElement(serviceListMob);
-    hideElement(typeServiceList);
+    showList(
+      locationServiceList,
+      serviceListMobUlTow,
+      "Ou ?",
+      "Chercher une ville",
+      listItemsTwo
+    );
+  });
 
-    titleService.textContent = "Ou ?";
-    inputService.placeholder = "Chercher une ville";
-
-    serviceListMobUlTow.innerHTML = "";
-    listItemsTwo.forEach((item) => {
-      const li = document.createElement("li");
-      li.textContent = item;
-      serviceListMobUlTow.appendChild(li);
+  // Hide lists on overlay or remove button click
+  [serviceListMobOverlay, removeServiceList].forEach((ele) => {
+    ele.addEventListener("click", () => {
+      hideAllLists();
+      document.body.classList.remove("no-scroll");
     });
-
-    scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-    document.body.classList.add("no-scroll");
   });
 
-  serviceListMobOverlay.addEventListener("click", () => {
-    hideElement(serviceListMob);
-    document.body.classList.remove("no-scroll");
-  });
-  removeServiceList.addEventListener("click", () => {
-    hideElement(serviceListMob);
-    document.body.classList.remove("no-scroll");
-  });
-
-  // render the list when the input changes
-  function renderList(input, ul, items) {
+  // Render filtered list
+  const renderList = (input, ul, items) => {
     input.addEventListener("input", () => {
-      let inputValue = input.value.toLowerCase();
-      // filter the list
-      let filteredItems = items.filter((item) =>
-        item.toLowerCase().includes(inputValue)
+      const inputValue = input.value.toLowerCase();
+      ul.innerHTML = items
+        .filter((item) => item.toLowerCase().includes(inputValue))
+        .map((item) => `<li>${item}</li>`)
+        .join("");
+      // Fill the input with the Item Text
+      ul.querySelectorAll("li").forEach((item) =>
+        item.addEventListener("click", (e) => {
+          input.value = e.target.innerHTML;
+        })
       );
-      // clear the ul
-      ul.innerHTML = "";
-      // insert the filtered list in the ul
-      filteredItems.forEach((item) => {
-        const li = document.createElement("li");
-        li.textContent = item;
-        ul.appendChild(li);
-      });
     });
-    // initial render
-    items.forEach((item) => {
-      const li = document.createElement("li");
-      li.textContent = item;
-      ul.appendChild(li);
-    });
-  }
+    ul.innerHTML = items.map((item) => `<li>${item}</li>`).join(""); // Initial render
+    // Fill the input with the Item Text
+    ul.querySelectorAll("li").forEach((item) =>
+      item.addEventListener("click", (e) => {
+        input.value = e.target.innerHTML;
+      })
+    );
+  };
+
+  // Render lists for desktop and mobile
   renderList(inputTypeService, typeServiceList, listItems);
   renderList(inputLocationService, locationServiceList, listItemsTwo);
   renderList(inputService, serviceListMobUl, listItems);
   renderList(inputService, serviceListMobUlTow, listItemsTwo);
 
-  // close the dropDownMenu when clicking on other parts
+  // Close lists when clicking outside
   window.addEventListener("click", (e) => {
-    if (e.target !== inputTypeService && e.target !== inputLocationService) {
-      hideElement(locationServiceList);
+    if (!e.target.closest(".type-service, .location-service, .list-appear")) {
       hideElement(typeServiceList);
+      hideElement(locationServiceList);
     }
   });
 });
 
-// scroll horizantallay with Click button in large view port
+// Horizontal Scroll for Containers
 document.addEventListener("DOMContentLoaded", () => {
   function horizontalScroll(containerSelector, arrowSelector) {
     const container = document.querySelector(containerSelector);
@@ -320,25 +297,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const scrollAmount = 600;
 
     if (container && leftArrow && rightArrow) {
-      leftArrow.addEventListener("click", () => {
-        container.scrollBy({
-          left: -scrollAmount,
-          behavior: "smooth",
-        });
-      });
-
-      rightArrow.addEventListener("click", () => {
-        container.scrollBy({
-          left: scrollAmount,
-          behavior: "smooth",
-        });
-      });
-    } else {
-      console.log(
-        `Elements not found for ${containerSelector} and ${arrowSelector}`
+      leftArrow.addEventListener("click", () =>
+        container.scrollBy({ left: -scrollAmount, behavior: "smooth" })
+      );
+      rightArrow.addEventListener("click", () =>
+        container.scrollBy({ left: scrollAmount, behavior: "smooth" })
       );
     }
   }
+
   horizontalScroll(".vendors-container", ".best-vendors");
   horizontalScroll(".discount-container", ".discount");
   horizontalScroll(".category-container", ".category-parent");
@@ -346,7 +313,7 @@ document.addEventListener("DOMContentLoaded", () => {
   horizontalScroll(".sugg-container", ".sugg-section");
 });
 
-// control the tabs in filter Section
+// Filter Section
 document.addEventListener("DOMContentLoaded", () => {
   const filterSection = document.querySelector(".filter-section");
   const filterIcon = document.querySelector(".filter-icon");
@@ -355,7 +322,7 @@ document.addEventListener("DOMContentLoaded", () => {
     ".filter-category div"
   );
   const filterOverlay = document.querySelector(".filter-overlay");
-  const rangeLabel = document.querySelectorAll(".filter-item label span");
+  const rangeLabels = document.querySelectorAll(".filter-item label span");
   const rangeInputs = document.querySelectorAll('input[type="range"]');
 
   filterCategoriesItem.forEach((category) => {
@@ -373,43 +340,34 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.classList.add("no-scroll");
   });
 
-  filterOverlay.addEventListener("click", () => {
-    hideElement(filterSection);
-    document.body.classList.remove("no-scroll");
-  });
-  closeFilterIcon.addEventListener("click", () => {
-    hideElement(filterSection);
-    document.body.classList.remove("no-scroll");
-  });
+  [closeFilterIcon, filterOverlay].forEach((el) =>
+    el.addEventListener("click", () => {
+      hideElement(filterSection);
+      document.body.classList.remove("no-scroll");
+    })
+  );
 
-  rangeInputs.forEach((input, ind) => {
-    // Update the background of the range input
+  rangeInputs.forEach((input, index) => {
     const updateBackground = () => {
       const value = ((input.value - input.min) / (input.max - input.min)) * 100;
       input.style.background = `linear-gradient(to right, var(--primary-color) ${value}%, #ddd ${value}%)`;
-      rangeLabel[ind].textContent = input.value;
+      rangeLabels[index].textContent = input.value;
     };
-
     // Initial background update
     updateBackground();
-
     // Update background on input change
     input.addEventListener("input", updateBackground);
   });
 });
 
-// control the like button
+// Like Button Toggle
 document.addEventListener("DOMContentLoaded", () => {
-  const likeBtns = document.querySelectorAll(".like-btn img");
-
-  likeBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      btn.classList.toggle("active");
-    });
+  document.querySelectorAll(".like-btn img").forEach((btn) => {
+    btn.addEventListener("click", () => btn.classList.toggle("active"));
   });
 });
 
-// control faq list
+// FAQ Section Toggle
 document.addEventListener("DOMContentLoaded", () => {
   const faqTitles = document.querySelectorAll(".faq-question");
 
@@ -417,56 +375,35 @@ document.addEventListener("DOMContentLoaded", () => {
     title.addEventListener("click", () => {
       const answer = title.nextElementSibling;
       const chevronIcon = title.querySelector("img");
-      if (answer.classList.contains("show")) {
-        answer.classList.remove("show");
-        chevronIcon.style.rotate = "0deg";
-      } else {
-        answer.classList.add("show");
-        chevronIcon.style.rotate = "180deg";
-      }
+      answer.classList.toggle("show");
+      chevronIcon.style.rotate = answer.classList.contains("show")
+        ? "180deg"
+        : "0deg";
     });
   });
 });
 
+// Contact Form Ready Prompt
 document.addEventListener("DOMContentLoaded", () => {
-  // control the contact form
   const textareaInput = document.querySelector(".contact-section textarea");
   const readyPromptList = document.querySelector(
     ".textarea-container .ready-prompt"
   );
-  const readyPromptListItems = document.querySelectorAll(
-    ".textarea-container .ready-prompt li"
-  );
 
-  textareaInput.addEventListener("focus", () => {
-    showElement(readyPromptList);
-  });
-  textareaInput.addEventListener("input", () => {
-    hideElement(readyPromptList);
-  });
+  textareaInput.addEventListener("focus", () => showElement(readyPromptList));
+  textareaInput.addEventListener("input", () => hideElement(readyPromptList));
 
   window.addEventListener("click", (e) => {
-    if (e.target !== readyPromptList && e.target !== textareaInput) {
+    if (![readyPromptList, textareaInput].includes(e.target))
       hideElement(readyPromptList);
-    }
   });
 
-  readyPromptListItems.forEach((item) => {
-    item.addEventListener("click", () => {
-      const itemValue = item.querySelector("p").innerText;
-      textareaInput.value = itemValue;
+  document
+    .querySelectorAll(".textarea-container .ready-prompt li")
+    .forEach((item) => {
+      item.addEventListener(
+        "click",
+        () => (textareaInput.value = item.querySelector("p").innerText)
+      );
     });
-  });
 });
-
-// show the calander
-// document.addEventListener("DOMContentLoaded", () => {
-//   const dateIcon = document.getElementById("date-icon");
-//   const dateInput = document.querySelector("#date");
-
-//   dateIcon.addEventListener("click", () => {
-//     console.log("hi");
-//     dateInput.focus();
-//     dateInput.click();
-//   });
-// });
