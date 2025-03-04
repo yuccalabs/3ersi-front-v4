4d12b16a318064e4730dea2eb8166539febdb4b5
djbayman <djaballahayman20@gmail.com>
2025-03-04 22:18:28 +0000

    add cake-patisserie page

--- assets/vendors/carousel.js
+++ assets/vendors/carousel.js
@@ -5,12 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const emblaHero = document.querySelector(".hero .embla");
   const emblaHero_slides = document.querySelectorAll(".hero .embla__slide");
 
-  const imgSrc = [
-    "/assets/images/hero-slider/1.jpg",
-    "/assets/images/hero-slider/2.jpg",
-    "/assets/images/hero-slider/3.jpg",
-    "/assets/images/hero-slider/4.jpg",
-  ];
+  const imgSrc = ["/assets/images/hero-slider/1.jpg", "/assets/images/hero-slider/2.jpg", "/assets/images/hero-slider/3.jpg", "/assets/images/hero-slider/4.jpg"];
 
   emblaHero_slides.forEach((slide, i) => {
     slide.style.backgroundImage = `url(${imgSrc[i]})`;
@@ -18,9 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
   if (emblaHero) {
     const viewportNode = emblaHero.querySelector(".embla__viewport");
     if (viewportNode) {
-      EmblaCarousel(viewportNode, OPTIONSHERO, [
-        EmblaCarouselAutoplay({ playOnInit: true, delay: 6000 }),
-      ]);
+      EmblaCarousel(viewportNode, OPTIONSHERO, [EmblaCarouselAutoplay({ playOnInit: true, delay: 6000 })]);
     } else {
       console.error("Viewport node not found");
     }
@@ -44,11 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (viewportNode) {
         const emblaApi = EmblaCarousel(viewportNode, OPTIONSVlIST);
 
-        const addPrevNextBtnsClickHandlers = (
-          emblaApi,
-          prevBtnNode,
-          nextBtnNode
-        ) => {
+        const addPrevNextBtnsClickHandlers = (emblaApi, prevBtnNode, nextBtnNode) => {
           const scrollPrev = () => emblaApi.scrollPrev();
           const scrollNext = () => emblaApi.scrollNext();
 
@@ -66,11 +55,7 @@ document.addEventListener("DOMContentLoaded", () => {
           const dots = Array.from({ length: slideLength }, (_, index) => {
             const dot = document.createElement("button");
             dot.classList.add("embla__dot");
-            dot.addEventListener(
-              "click",
-              () => emblaApi.scrollTo(index),
-              false
-            );
+            dot.addEventListener("click", () => emblaApi.scrollTo(index), false);
             dotsNode.appendChild(dot);
             return dot;
           });
@@ -86,25 +71,12 @@ document.addEventListener("DOMContentLoaded", () => {
           emblaApi.on("init", selectDot);
 
           return () => {
-            dots.forEach((dot) =>
-              dot.removeEventListener(
-                "click",
-                () => emblaApi.scrollTo(index),
-                false
-              )
-            );
+            dots.forEach((dot) => dot.removeEventListener("click", () => emblaApi.scrollTo(index), false));
           };
         };
 
-        const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
-          emblaApi,
-          prevBtnNode,
-          nextBtnNode
-        );
-        const removeDotBtnsClickHandlers = addDotBtnsAndClickHandlers(
-          emblaApi,
-          dotsNode
-        );
+        const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(emblaApi, prevBtnNode, nextBtnNode);
+        const removeDotBtnsClickHandlers = addDotBtnsAndClickHandlers(emblaApi, dotsNode);
         emblaApi.on("destroy", removePrevNextBtnsClickHandlers);
         emblaApi.on("destroy", removeDotBtnsClickHandlers);
       } else {
@@ -132,11 +104,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (viewportNode) {
         const emblaApi = EmblaCarousel(viewportNode, OPTIONSVlIST);
 
-        const addPrevNextBtnsClickHandlers = (
-          emblaApi,
-          prevBtnNode,
-          nextBtnNode
-        ) => {
+        const addPrevNextBtnsClickHandlers = (emblaApi, prevBtnNode, nextBtnNode) => {
           const scrollPrev = () => emblaApi.scrollPrev();
           const scrollNext = () => emblaApi.scrollNext();
 
@@ -153,11 +121,7 @@ document.addEventListener("DOMContentLoaded", () => {
           const dots = Array.from({ length: slideLength }, (_, index) => {
             const dot = document.createElement("button");
             dot.classList.add("embla__dot");
-            dot.addEventListener(
-              "click",
-              () => emblaApi.scrollTo(index),
-              false
-            );
+            dot.addEventListener("click", () => emblaApi.scrollTo(index), false);
             dotsNode.appendChild(dot);
             return dot;
           });
@@ -173,25 +137,78 @@ document.addEventListener("DOMContentLoaded", () => {
           emblaApi.on("init", selectDot);
 
           return () => {
-            dots.forEach((dot) =>
-              dot.removeEventListener(
-                "click",
-                () => emblaApi.scrollTo(index),
-                false
-              )
-            );
+            dots.forEach((dot) => dot.removeEventListener("click", () => emblaApi.scrollTo(index), false));
           };
         };
 
-        const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
-          emblaApi,
-          prevBtnNode,
-          nextBtnNode
-        );
-        const removeDotBtnsClickHandlers = addDotBtnsAndClickHandlers(
-          emblaApi,
-          dotsNode
-        );
+        const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(emblaApi, prevBtnNode, nextBtnNode);
+        const removeDotBtnsClickHandlers = addDotBtnsAndClickHandlers(emblaApi, dotsNode);
+
+        emblaApi.on("destroy", removePrevNextBtnsClickHandlers);
+        emblaApi.on("destroy", removeDotBtnsClickHandlers);
+      } else {
+        console.error("Viewport node not found");
+      }
+    });
+  } else {
+    console.error("Embla node not found");
+  }
+});
+// carousel for 3ersi-pro section
+document.addEventListener("DOMContentLoaded", () => {
+  const OPTIONSVlIST = { loop: true };
+
+  const emblaVList = document.querySelectorAll(".patisserie-occasion-section .embla");
+
+  if (emblaVList.length) {
+    emblaVList.forEach((card) => {
+      const viewportNode = card.querySelector(".embla__viewport");
+      const prevBtnNode = card.querySelector(".embla__button--prev");
+      const nextBtnNode = card.querySelector(".embla__button--next");
+      const dotsNode = card.querySelector(".embla__dots");
+
+      if (viewportNode) {
+        const emblaApi = EmblaCarousel(viewportNode, OPTIONSVlIST);
+
+        const addPrevNextBtnsClickHandlers = (emblaApi, prevBtnNode, nextBtnNode) => {
+          const scrollPrev = () => emblaApi.scrollPrev();
+          const scrollNext = () => emblaApi.scrollNext();
+
+          prevBtnNode.addEventListener("click", scrollPrev, false);
+          nextBtnNode.addEventListener("click", scrollNext, false);
+
+          return () => {
+            prevBtnNode.removeEventListener("click", scrollPrev, false);
+            nextBtnNode.removeEventListener("click", scrollNext, false);
+          };
+        };
+        const addDotBtnsAndClickHandlers = (emblaApi, dotsNode) => {
+          const slideLength = emblaApi.slideNodes().length;
+          const dots = Array.from({ length: slideLength }, (_, index) => {
+            const dot = document.createElement("button");
+            dot.classList.add("embla__dot");
+            dot.addEventListener("click", () => emblaApi.scrollTo(index), false);
+            dotsNode.appendChild(dot);
+            return dot;
+          });
+
+          const selectDot = () => {
+            const previous = emblaApi.previousScrollSnap();
+            const selected = emblaApi.selectedScrollSnap();
+            dots[previous].classList.remove("is-selected");
+            dots[selected].classList.add("is-selected");
+          };
+
+          emblaApi.on("select", selectDot);
+          emblaApi.on("init", selectDot);
+
+          return () => {
+            dots.forEach((dot) => dot.removeEventListener("click", () => emblaApi.scrollTo(index), false));
+          };
+        };
+
+        const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(emblaApi, prevBtnNode, nextBtnNode);
+        const removeDotBtnsClickHandlers = addDotBtnsAndClickHandlers(emblaApi, dotsNode);
 
         emblaApi.on("destroy", removePrevNextBtnsClickHandlers);
         emblaApi.on("destroy", removeDotBtnsClickHandlers);
